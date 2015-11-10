0de2dd3d7fe677e4cf2e944bf3f1722819a3565a
kuldeepshandilya <kuldeep.hbti@gmail.com>
2015-11-10 12:08:28 +0530

    Added images and data for articles on Site.

---
+++ data/footBallArticles.js
@@ -0,0 +1,67 @@
+
+/**
+ * Each article can use any combination of individual tags
+ */
+var tags = ['india','usa','Messi','Pele','Maradona','biography'];
+
+//TODO - replace article contents as per their title
+var articleText = [
+                   "It took nearly four decades for a batsman to score the first double-century in one-day international " +
+                   "cricket but less than two years for the second. Virender Sehwag, the batsman most touted to break Sachin" +
+                   " Tendulkar's record for the highest individual ODI score, didn't merely break it - he shattered it and " +
+                   "raised the bar so high that it's hard to imagine anyone, apart from Sehwag himself, raising it higher."+
+                   "Unlike Tendulkar in Gwalior, Sehwag wasn't running out of time as he raced towards 200 in Indore. He got" +
+                   " there in the 44th over and had made 219 off 149 balls by the time he was dismissed in the 47th." +
+                   " And in one of cricket's stranger coincidences, both ODI double-centuries were scored in the same " +
+                   "Indian state - Madhya Pradesh - at venues less than 500 kilometres apart.",
+
+					"It took nearly 40 years of waiting and it was well worth it. Sachin Tendulkar chose one of the better bowling attacks doing " +
+					"the rounds, to eclipse the record for the highest score, before bringing up the first double-hundred in ODI history. " +
+					"The spectators at the Captain Roop Singh Stadium became the envy of cricket fans as they witnessed one of the country's " +
+					"favourite sporting heroes play a breathtaking innings which not only set up a 153-run annihilation but also the series " +
+					"victory. He may have been run-out cheaply in the previous match, but nothing could deny him today - be it bowlers, fielders," +
+					" mix-ups or cramps. Dinesh Karthik, Yusuf Pathan and MS Dhoni stood by and admired as the master unfurled all the shots in" +
+					" his repertoire.",
+					
+					"It was sport – but not as America knows it.There was loud, vibrant noise emanating from the stands of Citi Field. The action" +
+					" was fast-paced and enjoyably engaging. Big hits boomed off the bat, acrobatic catches were held.This, however, wasn’t the World" +
+					" Series. It was cricket. Legends-style.Sachin Tendulkar and Shane Warne, two of the all-time greats, were attempting to stride where" +
+					" no superstar cricketer had dared venture before.The International Cricket Council (ICC) sees the potential of creating cash-rich" +
+					" revenue streams in the US while, so they claim, attempting to seriously establish the sport on these shores.Globalizing the game is" +
+					" also on the agenda.But despite a crowd of 30,000-plus, who ensured Queens was transformed into a cross between Mumbai and Karachi, the" +
+					" notion of getting your average New York sports fan excited about Ricky Ponting’s cover drive is fanciful to say the least.",
+					
+					"Sir Donald George 'Don' Bradman, AC (27 August 1908 – 25 February 2001), often referred to as 'The Don', was an Australian cricketer, " +
+					"widely acknowledged as the greatest Test batsman of all time.[2] Bradman's career Test batting average of 99.94 is often cited as the greatest" +
+					" achievement by any sportsman in any major sport.[3]The story that the young Bradman practised alone with a cricket stump and a golf ball is" +
+					" part of Australian folklore.[4] Bradman's meteoric rise from bush cricket to the Australian Test team took just over two years. Before his " +
+					"22nd birthday, he had set many records for top scoring, some of which still stand, and became Australia's sporting idol at the height of the" +
+					" Great Depression.During a 20-year playing career, Bradman consistently scored at a level that made him, in the words of former Australia captain" +
+					" Bill Woodfull, 'worth three batsmen to Australia'.[5] A controversial set of tactics, known as Bodyline, was specifically devised by the England " +
+					"team to curb his scoring. As a captain and administrator, Bradman was committed to attacking, entertaining cricket; he drew spectators in record numbers." +
+					" He hated the constant adulation, however, and it affected how he dealt with others. The focus of attention on his individual performances strained " +
+					"relationships with some team-mates, administrators and journalists, who thought him aloof and wary.[6] Following an enforced hiatus due to the Second " +
+					"World War, he made a dramatic comeback, captaining an Australian team known as 'The Invincibles' on a record-breaking unbeaten tour of England.",
+					
+					"Sachin Ramesh Tendulkar ; born 24 April 1973 is a former Indian cricketer and captain, widely regarded to be one of the greatest cricketers of all" +
+					" time and by many as the greatest batsman of all time.He took up cricket at the age of eleven, made his Test debut on 15 November 1989 against Pakistan" +
+					" in Karachi at the age of sixteen, and went on to represent Mumbai domestically and India internationally for close to twenty-four years. He is the only" +
+					" player to have scored one hundred international centuries, the first batsman to score a double century in a One Day International, holds the record for most" +
+					" number of runs in both ODI and Test cricket, the only player to complete more than 30,000 runs in international cricket.In 2002 just half way through his career," +
+					" Wisden Cricketers' Almanack ranked him the second greatest Test batsman of all time, behind Don Bradman, and the second greatest ODI batsman of all time, behind" +
+					" Viv Richards. Later in his career, Tendulkar was a part of the Indian team that won the 2011 World Cup, his first win in six World Cup appearances for India." +
+					" He had previously been named 'Player of the Tournament' at the 2003 edition of the tournament, held in South Africa. In 2013, he was the only Indian cricketer" +
+							" included in an all-time Test World XI named to mark the 150th anniversary of Wisden Cricketers' "
+                   ];
+
+/**
+ * Each article is of following structure
+ * {<tags>,<articleTitle>,<articleImage>,<articleText>,<author>,<date>}
+ */
+var footballArticles = [
+                       {'tags': [tags[0],tags[3]], 'title' : 'Messi', 'image': 'images/football/messi.png', 'text' : articleText[0], 'author' : 'Hareesh Bhimani', 'date' : '1st June 2013'},
+                       {'tags': [tags[0],tags[1],tags[2]], 'title' : 'World Cup 1983', 'image': 'images/football/fifa1983.jpg', 'text' : articleText[1], 'author' : 'Pankaj Kapur', 'date' : '2nd April 2014'},
+                       {'tags': [tags[1]], 'title' : 'USA Soccer history', 'image': 'images/football/soccerUSA.jpg', 'text' : articleText[2], 'author' : 'Steve Brenner', 'date' : '9th November 2015'},
+                       {'tags': [tags[0]], 'title' : 'Indian Soccer Journey', 'image': 'images/football/soccerIndia.jpg', 'text' : articleText[3], 'author' : 'Steve Brenner', 'date' : '9th November 2015'},
+                       {'tags': [tags[2],tags[5]], 'title' : 'Biography : Maradona', 'image': 'images/football/maradona.jpg', 'text' : articleText[4], 'author' : 'Suneel Wiki', 'date' : '9th November 2015'}
+                       ]
---
+++ data/tennisArticles.js
@@ -0,0 +1,88 @@
+
+/**
+ * Each article can use any combination of individual tags
+ */
+var tags = ['india','usa','Maria Sharapova','Roger Federer','boris','biography'];
+
+var articleText = [
+                   "Maria Yuryevna Sharapova; born 19 April 1987 is a Russian professional tennis player, who is ranked world No. 3" +
+                   " by the Women's Tennis Association (WTA). A United States resident since 1994,[4] Sharapova has competed on the " +
+                   "WTA tour since 2001. She has been ranked world No. 1 in singles by the WTA on five separate occasions, for a total" +
+                   " of 21 weeks. She is one of ten women, and the only Russian, to hold the career Grand Slam. She is also an Olympic" +
+                   " medalist, having earned silver for Russia in women's singles at the 2012 Summer Olympics in London.Sharapova became" +
+                   " the world No. 1 for the first time on August 22, 2005, at the age of 18, and last held the ranking for the fifth time" +
+                   " for four weeks from June 11, 2012, to July 8, 2012. Her 35 singles titles and five Grand Slam titles—two at the French" +
+                   " Open and one each at the Australian Open, Wimbledon, and US Open—rank third among active players, behind Serena and Venus" +
+                   " Williams. She won the year-ending WTA Finals in her debut in 2004. She has also won three doubles titles.Despite an injury-prone" +
+                   " career, Sharapova has achieved a rare level of longevity in the women’s game. She has won at least one singles title a year from" +
+                   " 2003 until 2015, a streak only bested by Steffi Graf, Martina Navratilova, and Chris Evert. Several tennis pundits and former players" +
+                   " have called Sharapova one of tennis’s best competitors, with John McEnroe calling her one of the best the sport has ever seen.Sharapova" +
+                   " has been featured in a number of modeling assignments, including a feature in the Sports Illustrated Swimsuit Issue. She appeared in" +
+                   " many advertisements, including those for Nike, Prince, and Canon, being the face of several fashion houses, most notably Cole Haan." +
+                   " Since February 2007, she has been a United Nations Development Programme Goodwill Ambassador, concerned specifically with the Chernobyl" +
+                   " Recovery and Development Programme. In June 2011, she was named one of the '30 Legends of Women's Tennis: Past, Present and Future' by" + 
+                   "Time and in March 2012 was named one of the '100 Greatest of All Time' by Tennis Channel.",
+
+					"Novak Djokovic defeated Roger Federer for the second straight year in a Wimbledon final, winning the tournament for the third" +
+					" time with a 7-6 (7-1) 6-7 (10-12), 6-4, 6-3 victory and rewarding himself by taking a bite out of Centre Court.Djokovic, ranked" +
+					"  No. 1 in the world, has now won nine Grand Slam tournaments and kept No. 2 Federer from becoming the oldest man to win Wimbledon" +
+					" at age 33. He regained his composure and rallied after Federer won an epic second-set tiebreak that left Djokovic yelling angrily" +
+					" from the bench. 'It's a dream come true again. Even though I've won it for a third time, every time it feels like (the) first," +
+					"' Djokovic said afterward. 'Today, I knew that Roger was going to come out being very aggressive. I knew he was going to play as he " +
+					"always plays in finals and majors -- at the highest possible level. If I wanted to have a chance to win, I needed to stay mentally tough." +
+					"'Unfortunately in the second-set tiebreak I had many, many opportunities, but that's why he's a champion. ... He's so calm and he fights 'til" +
+					" the last shot.'Asked what he was telling himself as he angrily slapped his feet with his racket and tried to rip his shirt after the second set," +
+					" Djokovic replied: 'You don't want to know. But I managed to bounce back.'When it was over Djokovic, who is coached by former German great Boris" +
+					" Becker, knelt and picked some grass to eat after the final point",
+					
+					"Leander Paes passed countryman Mahesh Bhupathi in claiming a men's record ninth Grand Slam mixed doubles title on Friday at the US Open." +
+					"Paes teamed with Martina Hingis to defeat Sam Querrey and Bethanie Mattek-Sands 6-4, 3-6, 10-7. The Indian-Swiss pair fired 32 winners and " +
+					"staved off four of five break points over the 77-minute affair. They rallied from an early 4-1 deficit in the Match Tie-break, claiming nine" +
+					" of the next 12 points to prevail.'Martina said something that was quite interesting,' said Paes. 'She said you have to have the guts to go" +
+					" after something, and I don't believe that I personally have the technique or the caliber of talent that Martina has.'One thing I do have is the" +
+					" guts. I will go for it. My whole life is about perseverance, just trying to find a way to succeed. Sometimes the chips are against you; sometimes" +
+					" they fool you. What I love about this teamwork is that in any partnership there's got to be one person who brings the energy to the team. There's" +
+					" got to be one person who takes all the pressure on their shoulder and drives the team forward.'I know if I can keep Martina happy, if I can keep her" +
+					" relaxed, the tennis I don't even have to worry about. This young girl is phenomenal on the tennis court and off of it. We actually have our best times" +
+					" when we are on the practice court. We were on the practice court before the Australian Open. We were on for like two hours practising for a final. Just" +
+					" having fun. I learn a lot from her.'",
+					
+					"The 2015 Australian Open was a tennis tournament that took place at Melbourne Park from 19 January to 1 February 2015. It was the 103rd edition" +
+					" of the Australian Open, and the first Grand Slam tournament of the year.Stanislas Wawrinka was the defending champion in men's singles but lost to" +
+					" four-time Australian Open champion Novak Djokovic in the semi-finals. Reigning women's champion Li Na did not defend her title, as she retired from" +
+					" professional tennis in September, 2014.[1] Novak Djokovic won an Open Era record fifth men's singles crown by defeating Andy Murray in the finals, and" +
+					" this was the third time they met each other in the finals. Serena Williams won an Open Era record six women's singles championships by defeating Maria " +
+					"Sharapova in the finals, and this was the second time they met each other in the finals. Simone Bolelli and Fabio Fognini teamed up to win the men's doubles" +
+					" title for the first time over the team of Pierre-Hugues Herbert and Nicolas Mahut.[4] Bethanie Mattek-Sands and Lucie Šafárová teamed up to win the women's" +
+					" doubles crown for the first time over the team of Chan Yung-jan and Zheng Jie.[5] Martina Hingis and Leander Paes teamed up to win the mixed doubles title, " +
+					"it was the second for Hingis and third for Paes, over the defending champions Kristina Mladenovic and Daniel Nestor.The 2015 Australian Open was the 103rd edition" +
+					" of the tournament and was held at Melbourne Park in Melbourne, Victoria, Australia.The tournament was run by the International Tennis Federation (ITF) and was" +
+					" be part of the 2015 ATP World Tour and the 2015 WTA Tour calendars under the Grand Slam category. The tournament consisted of both men's and women's singles" +
+					" and doubles draws as well as a mixed doubles event. There were singles and doubles events for both boys and girls (players under 18), which was part of the " +
+					"Grade A category of tournaments, and also singles, doubles and quad events for men's and women's wheelchair tennis players as part of the NEC tour under the Grand Slam category",
+					
+					"Boris Franz Becker  born 22 November 1967) is a German former world No. 1 professional tennis player. He is a six-time major singles champion," +
+					" including having been the youngest Wimbledon men's champion when he was 17. He also won 13 Masters Series titles, five elite indoor titles " +
+					"(three ATP Tour Finals, a WCT Finals, and a Grand Slam Cup), and an Olympic gold medal in doubles. Tennis magazine ranked Becker the 11th best male" +
+					" player of the period 1965–2005. He is currently coaching Novak Djokovic.Becker was born in Leimen, Germany, the only son of Elvira and Karl-Heinz Becker." +
+					" His mother was Catholic, and they raised him as a Catholic.[1][2] His father Karl-Heinz, an architect, founded a tennis centre in Leimen, where Becker learned" +
+					" the game.Becker turned professional in 1984, under the guidance of Romanian-born coach Günther Bosch and Romanian manager Ion Tiriac, and won his first professional" +
+					" doubles title that year in Munich. As a German teenager, Becker won the Tennis World Young Masters at the NEC in Birmingham in 1985, before taking his first top-level" +
+					" singles title in June that year at Queen's Club. Two weeks later, on 7 July, he became the first unseeded player and the first German to win the Wimbledon singles title," +
+					" defeating Kevin Curren in four sets. Unseeded, Becker was at that time ranked 20th in ATP ranking,[3] but Wimbledon did not then seed players beyond the top 16. He was the" +
+					" youngest ever male Grand Slam singles champion at 17 years, 227 days (a record later broken by Michael Chang in 1989, who won the French Open when he was 17 years, 110 days)." +
+					" Two months after his triumph, Becker became the youngest winner of the Cincinnati Open. Becker has since said that The plan from my parents for me was to finish school, go" +
+					"to university, get a proper degree and learn something respectful. The last thing on everyone's mind was me becoming a tennis professional"
+                   ];
+
+/**
+ * Each article is of following structure
+ * {<tags>,<articleTitle>,<articleImage>,<articleText>,<author>,<date>}
+ */
+var tennisArticles = [
+                       {'tags': [tags[0],tags[2]], 'title' : 'Maria Sharapova', 'image': 'images/tennis/mariasharapova.png', 'text' : articleText[0], 'author' : 'Hareesh Bhimani', 'date' : '1st June 2013'},
+                       {'tags': [tags[0],tags[1],tags[2]], 'title' : 'Wimbledon 2014 final', 'image': 'images/tennis/wimbledonfinal.jpg', 'text' : articleText[1], 'author' : 'Pankaj Kapur', 'date' : '2nd April 2014'},
+                       {'tags': [tags[0]], 'title' : 'US Mixed Doubles final 2015', 'image': 'images/tennis/USdoublesfinal.jpg', 'text' : articleText[2], 'author' : 'Steve Brenner', 'date' : '9th November 2015'},
+                       {'tags': [tags[0],tags[4],tags[5]], 'title' : 'Australian Open 2015', 'image': 'images/tennis/australianopen2015.jpg', 'text' : articleText[3], 'author' : 'Steve Brenner', 'date' : '9th November 2015'},
+                       {'tags': [tags[0],tags[1], tags[3],tags[5]], 'title' : 'Biography : Boris Becker', 'image': 'images/tennis/boris.jpg', 'text' : articleText[4], 'author' : 'Suneel Wiki', 'date' : '9th November 2015'}
+                       ]
